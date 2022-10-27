f964e0bb436747fb850293e1bb2cfa4e5529a00f
saneo <saneo@saneo_desktop>
2022-10-27 21:22:44 +0900

    login

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from crypt import methods
+from flask import *
 app= Flask(__name__)
-
 @app.route('/')
 def home():
-    return 'index.html'
+    return redirect('/login')
+    
+@app.route('/login_confirm', methods=['POST'])
+def login_confirm():
+    id = request.form['id']
+    pw = request.form['id']
+    if id == 'admin' and pw == 'admin':
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
 
+@app.route('/login')
+def login():
+    return render_template('/login.html')
+
+@app.route('/index')
+def index():
+    return render_template('/index.html')
 if __name__ == '__main__':
     app.run(debug=True)
