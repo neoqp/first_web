f51ea6828ce0a1844de35eabe8ee502bf319d8b5
saneo <neo6450@naver.com>
2022-11-02 21:02:41 +0900

    web

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # first_web
 cykor first website
+address : http://184.73.53.253:5000/
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import *
+import pymysql
 app= Flask(__name__)
 host = '0.0.0.0'
 port = '8080'
+
+
 @app.route('/')
 def root():
     return redirect('/home')
